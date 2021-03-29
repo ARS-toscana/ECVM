e84cc8c40a8d47607845b9c58bbbb50004696f7e
Davide <davide.messina@ars.toscana.it>
2021-03-29 17:55:56 +0200

    Create script

---
+++ to_run.R
@@ -0,0 +1,28 @@
+#-------------------------------
+# ECVM script
+# v0.1 - 20 March 2021
+# authors:
+
+
+rm(list=ls(all.names=TRUE))
+
+#set the directory where the file is saved as the working directory
+if (!require("rstudioapi")) install.packages("rstudioapi")
+thisdir<-setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+thisdir<-setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+
+#load parameters
+source(paste0(thisdir,"/p_parameters/01_parameters_program.R"))
+source(paste0(thisdir,"/p_parameters/02_parameters_CDM.R"))
+source(paste0(thisdir,"/p_parameters/03_concept_sets.R"))
+source(paste0(thisdir,"/p_parameters/04_itemsets.R"))
+source(paste0(thisdir,"/p_parameters/05_subpopulations_restricting_meanings.R"))
+source(paste0(thisdir,"/p_parameters/06_algorithms.R"))
+
+
+#run scripts
+
+# 01 RETRIEVE RECORDS FRM CDM
+
+system.time(source(paste0(thisdir,"/p_steps/step_01_1_T2.1_create_conceptset_datasets.R")))
