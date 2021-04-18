8987e20e8d57fa8326fdf636540665a23b7f45f4
Davide <davide.messina@ars.toscana.it>
2021-04-18 17:00:13 +0200

    Create study population

--- p_steps/step_04_T2_create_exclusion_criteria.R
+++ p_steps/step_04_T2_create_exclusion_criteria.R
@@ -27,25 +27,21 @@ for (i in 1:length(files)) {
 }
 
 #STANDARDIZE THE DATE FORMAT WITH  LUBRIDATE
-PERSONS<-PERSONS[,date_of_birth:=lubridate::ymd(with(PERSONS, paste(year_of_birth, month_of_birth, day_of_birth,sep="-")))]
-PERSONS<-suppressWarnings(PERSONS[,date_of_death:=lubridate::ymd(with(PERSONS, paste(year_of_death, month_of_death, day_of_death,sep="-")))])
+PERSONS<-PERSONS[, date_of_birth := lubridate::ymd(paste(year_of_birth, month_of_birth, day_of_birth, sep="-"))]
+PERSONS<-suppressWarnings(PERSONS[, date_of_death:=lubridate::ymd(paste(year_of_death, month_of_death, day_of_death, sep="-"))])
 
 #CONVERT SEX to BINARY 0/1
-PERSONS<-PERSONS[,sex:=as.numeric(ifelse(sex_at_instance_creation=="M",1,0))] #1:M 0:F
+PERSONS<-PERSONS[, sex := fifelse(sex_at_instance_creation == "M", 1, 0)] #1:M 0:F
 #[,age_at_index_date:=age_fast(date_of_birth,index_date)][age_at_index_date<12 | age_at_index_date>55,age:=1]
 
-PERSONS<-PERSONS[is.na(sex) | is.na(date_of_birth),sex_or_birth_date_missing:=1]
-PERSONS<-PERSONS[year(date_of_birth)<1899 | year(date_of_birth)>2021, birth_date_absurd:=1]
+PERSONS<-PERSONS[, sex_or_birth_date_missing:= fifelse(is.na(sex) | is.na(date_of_birth), 1, 0)]
+PERSONS<-PERSONS[, birth_date_absurd := fifelse(year(date_of_birth) < 1899 | year(date_of_birth) > 2021, 1, 0)]
 
 # no observation period (NA + )
-PERSONS_in_OP<-unique(merge(PERSONS, OBSERVATION_PERIODS, all.x = T, by="person_id")[is.na(op_start_date),no_observation_periods:=1][is.na(no_observation_periods),no_observation_periods:=0][,Minop_start_date:=min(no_observation_periods), by="person_id"][Minop_start_date==no_observation_periods,],by="person_id")
+PERSONS_in_OP <- merge(PERSONS, OBSERVATION_PERIODS, all.x = T, by="person_id")
+PERSONS_in_OP <- PERSONS_in_OP[, no_observation_period := fifelse(is.na(op_start_date), 1, 0)][, Minop_start_date := min(no_observation_period), by="person_id"][Minop_start_date==no_observation_period,]
 
-D3_exclusion_no_observation_periods<-PERSONS_in_OP[,.(person_id,sex_or_birth_date_missing,birth_date_absurd,no_observation_periods)]
-
-D3_study_source_population <- D3_exclusion_no_observation_periods[entry_spell_category >= study_start | exit_spell_category <= study_start, persons_not_in_study_start := 1]
-
-save(D3_study_source_population, file=paste0(dirtemp,"D3_study_source_population.RData"))
-
+D3_exclusion_no_observation_period <- unique(PERSONS_in_OP[,.(person_id, sex_or_birth_date_missing, birth_date_absurd, no_observation_period)])
 
 ## KEEP ONLY NEEDED VARs
 D3_inclusion_from_PERSONS <- PERSONS[,.(person_id,sex,date_of_birth,date_of_death)]
@@ -54,76 +50,35 @@ D3_inclusion_from_PERSONS <- PERSONS[,.(person_id,sex,date_of_birth,date_of_deat
 
 # OBSERVATION PERIODS -----------------------------------------------------
 
+start_follow_up = study_start - 365
+
 load(paste0(dirtemp,"output_spells_category.RData"))
 
 output_spells_category_enriched <- merge(output_spells_category,D3_inclusion_from_PERSONS, by="person_id")
-output_spells_category_enriched <- output_spells_category_enriched[entry_spell_category>date_of_birth,one_year_obs:=entry_spell_category+1*365][entry_spell_category<=date_of_birth,one_year_obs:=date_of_birth]
-
-## CALCULATE study_entry_date
-# output_spells_category_enriched <- output_spells_category_enriched[,study_entry_date:=max(date_of_birth,study_start,one_year_obs),by="person_id"]
-output_spells_category_enriched <- output_spells_category_enriched[,study_entry_date:=max(date_of_birth,study_start),by="person_id"]
-
-output_spells_category_enriched <- output_spells_category_enriched[date_of_birth>study_start & date_of_birth==entry_spell_category, study_entry_date:=date_of_birth]  #forse toglila
-
-#check if subject died before entering the study and correct if exit spell is wrong
-output_spells_category_enriched <-output_spells_category_enriched[!is.na(date_of_death) & date_of_death < study_entry_date, death_before_study_entry:=1]
-# TODO ask Rosa if want binary variable for quality check
-output_spells_category_enriched <-output_spells_category_enriched[!is.na(date_of_death) & date_of_death < exit_spell_category, exit_spell_category:=date_of_death]
-D3_exclusion_observation_periods_not_overlapping <-D3_exclusion_observation_periods_not_overlapping[is.na(death_before_study_entry),death_before_study_entry:=0]
-
-## KEEP ONLY SPELLS THAT INCLUDE study_entry_date AND WHOSE entry_spell_category IS < exit_spell_category
-output_spells_category_enriched <- output_spells_category_enriched[study_entry_date %between% list(entry_spell_category,exit_spell_category ) & entry_spell_category< exit_spell_category ,spell_contains_study_entry_date:=1, by="person_id"][is.na(spell_contains_study_entry_date),spell_contains_study_entry_date:=0]
-  
-D3_exclusion_observation_periods_not_overlapping<-output_spells_category_enriched[,spell_contains_study_entry_dateMAX:=max(spell_contains_study_entry_date), by="person_id"][,observation_periods_not_overlapping:=1-spell_contains_study_entry_dateMAX] #[,.(person_id,observation_periods_not_overlapping)])
-
-
-#check the subject has one year of lookback
-D3_exclusion_observation_periods_not_overlapping <- D3_exclusion_observation_periods_not_overlapping[one_year_obs>=study_entry_date ,insufficient_run_in:=1]
-D3_exclusion_observation_periods_not_overlapping[is.na(insufficient_run_in),insufficient_run_in:=0]
-  
-  
-D3_exclusion_observation_periods_not_overlapping <-merge(PERSONS[,.(person_id)],D3_exclusion_observation_periods_not_overlapping,by="person_id", all.x = T)[is.na(observation_periods_not_overlapping), observation_periods_not_overlapping := 1]
-  
-
-D3_exclusion_observation_periods_not_overlapping <-D3_exclusion_observation_periods_not_overlapping[,.(person_id,observation_periods_not_overlapping, insufficient_run_in, death_before_study_entry, study_entry_date)]
-  
-  
-## KEEP ONLY NEEDES VARs
-D3_inclusion_from_OBSERVATION_PERIODS <- output_spells_category_enriched[spell_contains_study_entry_date==1,.(person_id,exit_spell_category)]
-
-D3_inclusion_from_OBSERVATION_PERIODS <-merge(PERSONS[,.(person_id)],D3_inclusion_from_OBSERVATION_PERIODS,by="person_id", all.x = T)
+output_spells_category_enriched <- output_spells_category_enriched[, death_before_study_entry := fifelse(!is.na(date_of_death) & date_of_death < study_start, 1, 0)]
+output_spells_category_enriched <- output_spells_category_enriched[, no_observation_period_including_study_start := min(fifelse(study_start %between% list(entry_spell_category,exit_spell_category) & entry_spell_category < exit_spell_category, 0, 1)), by="person_id"]
+output_spells_category_enriched <- output_spells_category_enriched[, insufficient_run_in := fifelse(entry_spell_category > date_of_birth & entry_spell_category <= study_start - 365, 0, 1)]
+output_spells_category_enriched <- output_spells_category_enriched[, insufficient_run_in := min(fifelse(entry_spell_category == date_of_birth & year(date_of_birth) == 2019, 0 , insufficient_run_in)), by="person_id"]
+output_spells_category_enriched <- output_spells_category_enriched[, study_entry_date := study_start][, start_follow_up := start_follow_up][, study_exit_date:= min(date_of_death, exit_spell_category, study_end, na.rm = T), by="person_id"]
+D3_exclusion_observation_periods_not_overlapping <- unique(output_spells_category_enriched[,.(person_id, death_before_study_entry, insufficient_run_in, no_observation_period_including_study_start)])
 
 PERSONS_OP <- merge(D3_inclusion_from_PERSONS,
-                    D3_exclusion_no_observation_periods,
+                    D3_exclusion_no_observation_period,
                     by="person_id",
                     all.x = T)
 PERSONS_OP2 <- merge(PERSONS_OP,
-                     D3_inclusion_from_OBSERVATION_PERIODS,
-                     by="person_id",
-                     all.x = T)
-PERSONS_OP3 <- merge(PERSONS_OP2,
                      D3_exclusion_observation_periods_not_overlapping,
                      by="person_id",
                      all.x = T)
   
-coords<-c("sex_or_birth_date_missing", "birth_date_absurd", "no_observation_periods", "insufficient_run_in","observation_periods_not_overlapping", "death_before_study_entry")
-PERSONS_OP3[, (coords) := replace(.SD, is.na(.SD), 0), .SDcols = coords]
-  
-# CREATE study_exit_date
-selcriteria <- PERSONS_OP3[,study_exit_date:=min(date_of_death, exit_spell_category, study_end, na.rm = T), by="person_id"]
-D3_selection_criteria_doses <- selcriteria
-  # if (this_datasource_has_subpopulations == FALSE){ 
-  #   D3_selection_criteria <- selcriteria
-  # }else{
-  #   D3_selection_criteria[[subpop]] <- selcriteria
-  # }
-  # 
+coords<-c("sex_or_birth_date_missing", "birth_date_absurd", "no_observation_period", "insufficient_run_in","no_observation_period_including_study_start", "death_before_study_entry")
+D3_selection_criteria_doses <- PERSONS_OP2[, (coords) := replace(.SD, is.na(.SD), 0), .SDcols = coords]
 
 save(D3_selection_criteria_doses,file=paste0(dirtemp,"D3_selection_criteria_doses.RData"))
 
 rm(output_spells_category_enriched,D3_inclusion_from_OBSERVATION_PERIODS,D3_inclusion_from_PERSONS,D3_exclusion_observation_periods_not_overlapping,selcriteria)
 rm(PERSONS_OP, PERSONS_OP2, PERSONS_OP3)
-rm(PERSONS, PERSONS_in_OP, output_spells_category,OBSERVATION_PERIODS, D3_selection_criteria_doses, D3_exclusion_no_observation_periods)
+rm(PERSONS, PERSONS_in_OP, output_spells_category,OBSERVATION_PERIODS, D3_selection_criteria_doses, D3_exclusion_no_observation_period)
 
 
 
--- p_steps/step_05_T3_apply_exclusion_criteria.R
+++ p_steps/step_05_T3_apply_exclusion_criteria.R
@@ -9,7 +9,7 @@ print('FLOWCHART')
 #USE THE FUNCTION CREATEFLOWCHART TO SELECT THE SUBJECTS IN POPULATION
 
 # subpopulations_non_empty <- c()
-
+D4_study_source_population <- vector(mode="list")
 D4_study_population <- vector(mode="list")
 # if (this_datasource_has_subpopulations == TRUE){ 
 #   D4_study_population <- vector(mode="list")
@@ -19,8 +19,8 @@ load(paste0(dirtemp,"D3_selection_criteria_doses.RData"))
 
 selected_population <- CreateFlowChart(
   dataset = D3_selection_criteria_doses,
-  listcriteria = c("sex_or_birth_date_missing", "birth_date_absurd", "no_observation_periods", "insufficient_run_in","observation_periods_not_overlapping", "death_before_study_entry"),
-  flowchartname = "FlowChart")
+  listcriteria = c("sex_or_birth_date_missing", "birth_date_absurd", "no_observation_period", "death_before_study_entry", "no_observation_period_including_study_start"),
+  flowchartname = "Flowchart_basic_exclusion_criteria")
 
 suppressWarnings(
   if(!(file.exists(direxp))) {dir.create(file.path(direxp))}
@@ -29,56 +29,25 @@ suppressWarnings(
   if(!(file.exists(diroutput))) {dir.create(file.path(diroutput))}
 )
 
-fwrite(FlowChart, paste0(direxp,"FlowChart.csv"))
+fwrite(Flowchart_basic_exclusion_criteria, paste0(direxp,"Flowchart_basic_exclusion_criteria.csv"))
 
-# for (subpop in subpopulations[[thisdatasource]]){ 
-#   load(paste0(dirtemp,"D3_selection_criteria.RData"))
-#     if (this_datasource_has_subpopulations == TRUE)D3_selection_criteria <- D3_selection_criteria[[subpop]]
-#   
-#   selected_population <- CreateFlowChart(
-#     dataset = D3_selection_criteria,
-#     listcriteria = c("sex_or_birth_date_missing","birth_date_absurd","no_op_start_date","death_before_study_entry","observed_time_no_overlap","insufficient_run_in"),
-#     flowchartname = "FlowChart" )
-  
-  
-  # # D4_study_population contains the starting information on age and days of follow up per each patient
-  # selected_population <- unique(selected_population[,.(person_id,sex,date_of_birth,study_entry_date,study_exit_date)])
-  # 
-  # if (this_datasource_has_subpopulations == TRUE & nrow(selected_population)>0){
-  #   subpopulations_non_empty <- c(subpopulations_non_empty,subpop)
-  #   
-  #   D4_study_population[[subpop]] <- selected_population
-  #   fwrite(FlowChart, paste0(direxpsubpop[[subpop]],"FlowChart.csv"))  }  
-  # 
-  # if (this_datasource_has_subpopulations == FALSE & nrow(selected_population)>0){ 
-  #   fwrite(FlowChart, paste0(direxp,"FlowChart.csv"))
-  #   D4_study_population <- selected_population
-  #   subpopulations_non_empty <- c(subpopulations_non_empty,subpop)
-  #   }
-# }
-# save(subpopulations_non_empty,file=paste0(dirpargen,"subpopulations_non_empty.RData"))
-D4_study_population_doses <- unique(selected_population)
-save(D4_study_population_doses,file=paste0(diroutput,"D4_study_population_doses.RData"))
+D4_study_source_population <- unique(selected_population)
+save(D4_study_source_population,file = paste0(diroutput,"D4_study_source_population.RData"))
+
+
+selected_population_all_filter <- CreateFlowChart(
+  dataset = selected_population[,.(person_id, sex, date_of_birth, date_of_death, insufficient_run_in)],
+  listcriteria = c("insufficient_run_in"),
+  flowchartname = "Flowchart_exclusion_criteria")
+
+fwrite(Flowchart_exclusion_criteria, paste0(direxp,"Flowchart_exclusion_criteria.csv"))
+
+D4_study_population <- unique(selected_population_all_filter)
+save(D4_study_population,file = paste0(diroutput, "D4_study_population.RData"))
 
 # rm(PERSONS, OBSERVATION_PERIODS)
-rm(D3_selection_criteria_doses,D4_study_population_doses,FlowChart, selected_population)
+rm(D3_selection_criteria_doses, D4_study_population, D4_study_source_population,
+   Flowchart_basic_exclusion_criteria, Flowchart_exclusion_criteria, selected_population)
 
 
-# mask small counts
-# for (subpop in subpopulations_non_empty){ 
-#   col <- c('N')  
-#   temp<-paste0(col,"=5")  
-#   temp2<-paste("c(",paste(temp, collapse = ','),")")
-#   thisdirexp <- ifelse(this_datasource_has_subpopulations == FALSE,direxp,direxpsubpop[[subpop]])
-#   thisdirsmallcountsremoved <- ifelse(this_datasource_has_subpopulations == FALSE,dirsmallcountsremoved,dirsmallcountsremovedsubpop[[subpop]])
-#   suppressWarnings(
-#     DRE_Treshold(
-#       Inputfolder = thisdirexp,
-#       Outputfolder = thisdirsmallcountsremoved,
-#       Delimiter = ",",
-#       Varlist = c(eval(parse(text=(temp2)))),
-#       FileContains = "FlowChart" 
-#     )
-#   )
-# }
 
