c4b1b56125fefb57d6a577da29ec5b93ffda58c1
Olga Paoletti <olga.paoletti@ars.toscana.it>
2021-04-21 19:56:48 +0200

    corrected bug

--- p_steps/step_04_2_T3_apply_quality_check_exclusion_criteria_doses.R
+++ p_steps/step_04_2_T3_apply_quality_check_exclusion_criteria_doses.R
@@ -1,13 +1,4 @@
 
-#merge vaccine information to persons
-
-load(paste0(dirtemp,"D3_selection_criteria_doses.RData"))
-load(paste0(dirtemp,"D3_concepts_QC_criteria.RData"))
-
-persons_doses<-merge(D3_selection_criteria_doses,D3_concepts_QC_criteria, by=c("person_id"),all = T)
-
-save(persons_doses,file=paste0(dirtemp,"persons_doses.RData"))
-
 load(paste0(dirtemp,"persons_doses.RData"))
 
 #add quality checks davide
