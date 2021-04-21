0051fa5c51bb2bf360e84a2d2dcd6727d2eabc7c
Olga Paoletti <olga.paoletti@ars.toscana.it>
2021-04-21 17:43:37 +0200

    debug

--- to_run.R
+++ to_run.R
@@ -3,7 +3,6 @@
 # v0.3 - 20 April 2021
 # authors:
 
-
 rm(list=ls(all.names=TRUE))
 
 #set the directory where the file is saved as the working directory
@@ -24,13 +23,20 @@ source(paste0(thisdir,"/p_parameters/05_subpopulations_restricting_meanings.R"))
 #run scripts
 
 # 01 RETRIEVE RECORDS FRM CDM
-
 system.time(source(paste0(thisdir,"/p_steps/step_01_1_T2.1_create_conceptset_datasets.R")))
 system.time(source(paste0(thisdir,"/p_steps/step_01_2_T2.1_create_spells.R")))
+
+#02 qualirt checks
 system.time(source(paste0(thisdir,"/p_steps/step_02_T2_create_exclusion_criteria_VACCINES.R")))
+
+#03 create exclution criteria
 system.time(source(paste0(thisdir,"/p_steps/step_03_T2_create_exclusion_criteria.R")))
+
+#04 apply exclutioc criteria
 system.time(source(paste0(thisdir,"/p_steps/step_04_T3_apply_exclusion_criteria.R")))
 system.time(source(paste0(thisdir,"/p_steps/step_04_1_T3_apply_single_exclusion_criteria.R")))
+
+#05 create D3 and D4
 system.time(source(paste0(thisdir,"/p_steps/step_05_1_T3_create_D3_datasets.R")))
 # system.time(source(paste0(thisdir,"/p_steps/step_05_2_T3_create_D4_doses_weeks.R")))
 system.time(source(paste0(thisdir,"/p_steps/step_05_3_T3_create_dashboard_tables.R")))
