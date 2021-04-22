722df1669196ba8153d6ba3494215fa6e9f356d8
Olga Paoletti <olga.paoletti@ars.toscana.it>
2021-04-22 11:13:59 +0200

    Bug step 03_2

--- p_steps/step_03_2_T2_merge_persons_concept.R
+++ p_steps/step_03_2_T2_merge_persons_concept.R
@@ -6,6 +6,8 @@ load(paste0(dirtemp,"D3_concepts_QC_criteria.RData"))
 
 persons_doses<-merge(D3_selection_criteria_doses,D3_concepts_QC_criteria, by=c("person_id"),all=T)
 
+persons_doses<-persons_doses[is.na(sex_or_birth_date_missing),sex_or_birth_date_missing:=1]
+
 temp <- copy(D3_selection_criteria_doses)[, .(person_id, date_of_death)]
 temp1 <- copy(D3_concepts_QC_criteria)[, .(person_id, date, vx_dose)]
 load(paste0(dirtemp,"output_spells_category.RData"))
