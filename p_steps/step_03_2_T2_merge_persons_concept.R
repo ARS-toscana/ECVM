13e837a9f9b1790554ce4b4ac5054650cec72ed4
Olga Paoletti <olga.paoletti@ars.toscana.it>
2021-04-23 16:09:18 +0200

    Corrected step 04_1 for subpopulations

--- p_steps/step_03_2_T2_merge_persons_concept.R
+++ p_steps/step_03_2_T2_merge_persons_concept.R
@@ -1,15 +1,21 @@
 
+
 #merge vaccine information to persons
+for (subpop in subpopulations[[thisdatasource]]){
+  print(subpop)
+  load(paste0(dirtemp,"output_spells_category.RData"))
+  load(paste0(dirtemp,"D3_selection_criteria.RData"))
+  load(paste0(dirtemp,"D3_concepts_QC_criteria.RData"))
+  
+  if (this_datasource_has_subpopulations == TRUE)output_spells_category <- as.data.table(output_spells_category[[subpop]])
+  if (this_datasource_has_subpopulations == TRUE)D3_selection_criteria <- as.data.table(D3_selection_criteria[[subpop]]) 
 
-load(paste0(dirtemp,"D3_selection_criteria_doses.RData"))
-load(paste0(dirtemp,"D3_concepts_QC_criteria.RData"))
-load(paste0(dirtemp,"output_spells_category.RData"))
 
-persons_doses<-merge(D3_selection_criteria_doses,D3_concepts_QC_criteria, by=c("person_id"),all=T)
+persons_doses<-merge(D3_selection_criteria,D3_concepts_QC_criteria, by=c("person_id"),all=T)
 
 persons_doses<-persons_doses[is.na(sex_or_birth_date_missing),sex_or_birth_date_missing:=1]
 
-temp <- copy(D3_selection_criteria_doses)[, .(person_id, date_of_death)]
+temp <- copy(D3_selection_criteria)[, .(person_id, date_of_death)]
 temp1 <- copy(D3_concepts_QC_criteria)[, .(person_id, date, vx_dose)]
 names(output_spells_category)
 temp2 <- copy(output_spells_category)[, .(person_id, entry_spell_category, exit_spell_category)]
@@ -28,6 +34,14 @@ for (i in names(temp_tot)){
 
 persons_doses <- merge(persons_doses, unique(temp_tot), by = c("person_id", "vx_dose"),all.x=T)
 
+if (this_datasource_has_subpopulations == FALSE){ 
+  persons_doses <- persons_doses
+}else{
+  persons_doses[[subpop]] <- persons_doses
+}
+}
+
+
 save(persons_doses,file=paste0(dirtemp,"persons_doses.RData"))
 
-rm(D3_selection_criteria_doses, D3_concepts_QC_criteria, persons_doses, temp, temp1, output_spells_category, temp2, temp_tot)
+rm(D3_selection_criteria, D3_concepts_QC_criteria, persons_doses, temp, temp1, output_spells_category, temp2, temp_tot)
--- p_steps/step_04_1_T3_apply_exclusion_criteria.R
+++ p_steps/step_04_1_T3_apply_exclusion_criteria.R
@@ -8,51 +8,74 @@ print('FLOWCHART')
 
 #USE THE FUNCTION CREATEFLOWCHART TO SELECT THE SUBJECTS IN POPULATION
 
-# subpopulations_non_empty <- c()
-D4_study_source_population <- vector(mode="list")
-D4_study_population <- vector(mode="list")
-# if (this_datasource_has_subpopulations == TRUE){ 
-#   D4_study_population <- vector(mode="list")
-# }
-
-load(paste0(dirtemp,"D3_selection_criteria_doses.RData"))
+subpopulations_non_empty <- c()
 
 
+if (this_datasource_has_subpopulations == TRUE){ 
+  D4_study_population <- vector(mode="list")
+  D4_study_source_population <- vector(mode="list")
+}
 
-selected_population <- CreateFlowChart(
-  dataset = D3_selection_criteria_doses,
-  listcriteria = c("sex_or_birth_date_missing", "birth_date_absurd", "no_observation_period", "death_before_study_entry", "no_observation_period_including_study_start"),
-  flowchartname = "Flowchart_basic_exclusion_criteria")
+for (subpop in subpopulations[[thisdatasource]]){ 
+  print(subpop)
+  load(paste0(dirtemp,"D3_selection_criteria.RData"))
+  if (this_datasource_has_subpopulations == TRUE)D3_selection_criteria <- D3_selection_criteria[[subpop]]
+  
+  selected_population <- CreateFlowChart(
+    dataset = D3_selection_criteria,
+    listcriteria = c("sex_or_birth_date_missing", "birth_date_absurd", "no_observation_period", "death_before_study_entry", "no_observation_period_including_study_start"),
+    flowchartname = "Flowchart_basic_exclusion_criteria")
+  
+  suppressWarnings(
+    if(!(file.exists(direxp))) {dir.create(file.path(direxp))}
+  )
+  suppressWarnings(
+    if(!(file.exists(diroutput))) {dir.create(file.path(diroutput))}
+  )
+  
+  
+  to_study_pop <- unique(selected_population)[, .(person_id, sex, date_of_birth, date_of_death,study_entry_date,start_follow_up, study_exit_date, insufficient_run_in)]
+  
+  D4_study_source_population <- to_study_pop[, .(person_id, sex, date_of_birth, date_of_death, study_entry_date,start_follow_up, study_exit_date)]
+  
+  if (this_datasource_has_subpopulations == TRUE & nrow(selected_population)>0){
+    
+    D4_study_source_population[[subpop]] <- selected_population
+    fwrite(Flowchart_basic_exclusion_criteria, paste0(direxpsubpop[[subpop]],"Flowchart_basic_exclusion_criteria.csv"))}  
+  
+  if (this_datasource_has_subpopulations == FALSE & nrow(selected_population)>0){ 
+    fwrite(Flowchart_basic_exclusion_criteria, paste0(direxp,"Flowchart_basic_exclusion_criteria.csv"))
+    D4_study_source_population <- selected_population
+  }
+  
+  
+  selected_population_all_filter <- CreateFlowChart(
+    dataset = selected_population[,.(person_id, sex, date_of_birth, date_of_death, study_entry_date, start_follow_up, study_exit_date, insufficient_run_in)],
+    listcriteria = c("insufficient_run_in"),
+    flowchartname = "Flowchart_exclusion_criteria")
+  
+  D4_study_population <- unique(selected_population_all_filter)[, .(person_id, sex, date_of_birth, date_of_death, study_entry_date,
+                                                                    start_follow_up, study_exit_date)]
+  
+  if (this_datasource_has_subpopulations == TRUE & nrow(selected_population_all_filter)>0){
+    subpopulations_non_empty <- c(subpopulations_non_empty,subpop)
+    
+    D4_study_population[[subpop]] <- selected_population_all_filter
+    fwrite(Flowchart_exclusion_criteria, paste0(direxpsubpop[[subpop]],"Flowchart_exclusion_criteria.csv"))}  
+  
+  if (this_datasource_has_subpopulations == FALSE & nrow(selected_population_all_filter)>0){ 
+    fwrite(Flowchart_exclusion_criteria, paste0(direxp,"Flowchart_exclusion_criteria.csv"))
+    D4_study_population <- selected_population_all_filter
+    subpopulations_non_empty <- c(subpopulations_non_empty,subpop)
+  }
+}
 
-suppressWarnings(
-  if(!(file.exists(direxp))) {dir.create(file.path(direxp))}
-)
-suppressWarnings(
-  if(!(file.exists(diroutput))) {dir.create(file.path(diroutput))}
-)
-
-fwrite(Flowchart_basic_exclusion_criteria, paste0(direxp,"Flowchart_basic_exclusion_criteria.csv"))
-
-to_study_pop <- unique(selected_population)[, .(person_id, sex, date_of_birth, date_of_death, study_entry_date,
-                                                start_follow_up, study_exit_date, insufficient_run_in)]
-D4_study_source_population <- to_study_pop[, .(person_id, sex, date_of_birth, date_of_death, study_entry_date,
-                                              start_follow_up, study_exit_date)]
-
+save(D4_study_population,file = paste0(diroutput, "D4_study_population.RData"))
+save(subpopulations_non_empty,file=paste0(dirpargen,"subpopulations_non_empty.RData"))
 save(D4_study_source_population,file = paste0(diroutput,"D4_study_source_population.RData"))
 
-selected_population_all_filter <- CreateFlowChart(
-  dataset = selected_population[,.(person_id, sex, date_of_birth, date_of_death, study_entry_date, start_follow_up, study_exit_date, insufficient_run_in)],
-  listcriteria = c("insufficient_run_in"),
-  flowchartname = "Flowchart_exclusion_criteria")
 
-fwrite(Flowchart_exclusion_criteria, paste0(direxp,"Flowchart_exclusion_criteria.csv"))
-
-D4_study_population <- unique(selected_population_all_filter)[, .(person_id, sex, date_of_birth, date_of_death, study_entry_date,
-                                                                  start_follow_up, study_exit_date)]
-save(D4_study_population,file = paste0(diroutput, "D4_study_population.RData"))
-
-# rm(PERSONS, OBSERVATION_PERIODS)
-rm(D4_study_source_population, D4_study_population, D3_selection_criteria_doses, Flowchart_basic_exclusion_criteria,
+rm(D4_study_source_population, D4_study_population, D3_selection_criteria, Flowchart_basic_exclusion_criteria,
    Flowchart_exclusion_criteria, selected_population, to_study_pop, selected_population_all_filter)
 
 
