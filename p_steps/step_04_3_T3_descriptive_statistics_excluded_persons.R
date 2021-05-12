208436ce6388d712448519f73048717432b6788b
Davide <davide.messina@ars.toscana.it>
2021-05-12 21:19:02 +0200

    Added percentage in descr excluded pop

--- p_steps/step_04_3_T3_descriptive_statistics_excluded_persons.R
+++ p_steps/step_04_3_T3_descriptive_statistics_excluded_persons.R
@@ -6,7 +6,7 @@ population_ins_run_in <- copy(D3_selection_criteria)[sex_or_birth_date_missing =
 
 population_no_obs_start <- copy(D3_selection_criteria)[sex_or_birth_date_missing == 0 & birth_date_absurd == 0 &
                                                          no_observation_period == 0 & death_before_study_entry == 0 & 
-                                                         no_observation_period_including_study_start == 1 & insufficient_run_in == 0, ]
+                                                         no_observation_period_including_study_start == 1, ]
 
 population_ins_run_in <- population_ins_run_in[, .(person_id, sex, date_of_birth)]
 population_ins_run_in <- population_ins_run_in[, date_of_birth := findInterval(year(date_of_birth), c(1940, 1950, 1960, 1970, 1980, 1990))]
@@ -48,8 +48,66 @@ for (i in names(pop_excluded)){
   pop_excluded[is.na(get(i)), (i):=0]
 }
 
+tot_population_ins_run_in <- copy(D3_selection_criteria)[sex_or_birth_date_missing == 0 & birth_date_absurd == 0 & 
+                                                       no_observation_period == 0 & death_before_study_entry == 0 & 
+                                                       no_observation_period_including_study_start == 0, ]
+
+tot_population_no_obs_start <- copy(D3_selection_criteria)[sex_or_birth_date_missing == 0 & birth_date_absurd == 0 &
+                                                         no_observation_period == 0 & death_before_study_entry == 0, ]
+
+
+tot_population_ins_run_in <- tot_population_ins_run_in[, .(person_id, sex, date_of_birth)]
+tot_population_ins_run_in <- tot_population_ins_run_in[, date_of_birth := findInterval(year(date_of_birth), c(1940, 1950, 1960, 1970, 1980, 1990))]
+tot_population_ins_run_in$date_of_birth <- as.character(tot_population_ins_run_in$date_of_birth)
+tot_population_ins_run_in <- tot_population_ins_run_in[.(date_of_birth = c("0", "1", "2", "3", "4", "5", "6"),
+                                                 to = c("<1940", "1940-1949", "1950-1959", "1960-1969", "1970-1979",
+                                                        "1980-1989", "1990+")),
+                                               on = "date_of_birth", date_of_birth := i.to]
+
+tot_population_no_obs_start <- tot_population_no_obs_start[, .(person_id, sex, date_of_birth)]
+tot_population_no_obs_start <- tot_population_no_obs_start[, date_of_birth := findInterval(year(date_of_birth), c(1940, 1950, 1960, 1970, 1980, 1990))]
+tot_population_no_obs_start$date_of_birth <- as.character(tot_population_no_obs_start$date_of_birth)
+tot_population_no_obs_start <- tot_population_no_obs_start[.(date_of_birth = c("0", "1", "2", "3", "4", "5", "6"),
+                                                     to = c("<1940", "1940-1949", "1950-1959", "1960-1969", "1970-1979",
+                                                            "1980-1989", "1990+")),
+                                                   on = "date_of_birth", date_of_birth := i.to]
+
+tot_population_ins_run_in_sex <- unique(copy(tot_population_ins_run_in)[, N := .N, by = c("sex")][, c("person_id", "date_of_birth") := NULL])
+tot_population_ins_run_in_date <- unique(tot_population_ins_run_in[, N := .N, by = c("date_of_birth")][, c("person_id", "sex") := NULL])
+tot_population_ins_run_in_sex <- tot_population_ins_run_in_sex[ , variable := "ins_run_in"]
+tot_population_ins_run_in_date <- tot_population_ins_run_in_date[ , variable := "ins_run_in"]
+
+tot_population_no_obs_start_sex <- unique(copy(tot_population_no_obs_start)[, N := .N, by = c("sex")][, c("person_id", "date_of_birth") := NULL])
+tot_population_no_obs_start_date <- unique(tot_population_no_obs_start[, N := .N, by = c("date_of_birth")][, c("person_id", "sex") := NULL])
+tot_population_no_obs_start_sex <- tot_population_no_obs_start_sex[ , variable := "no_obs_in_start"]
+tot_population_no_obs_start_date <- tot_population_no_obs_start_date[ , variable := "no_obs_in_start"]
+
+tot_population_sex <- rbind(tot_population_ins_run_in_sex, tot_population_no_obs_start_sex)
+tot_population_date <- rbind(tot_population_ins_run_in_date, tot_population_no_obs_start_date)
+
+tot_population_sex <- dcast(tot_population_sex, sex ~ variable, value.var = "N")
+tot_population_date <- dcast(tot_population_date, date_of_birth ~ variable, value.var = "N")
+setnames(tot_population_sex, "sex", "")
+setnames(tot_population_date, "date_of_birth", "")
+
+tot_pop_excluded <- rbind(tot_population_sex, tot_population_date)
+
+to_ord <- c(1, 2, 4, 5, 6, 7, 8, 9, 3)
+setorder(tot_pop_excluded[, .r := to_ord], .r)[, .r := NULL]
+setorder(pop_excluded[, .r := to_ord], .r)[, .r := NULL]
+
+for (i in names(tot_pop_excluded)){
+  tot_pop_excluded[is.na(get(i)), (i):=0]
+}
+
+for (i in names(tot_pop_excluded)[2:3]){
+  pop_excluded[, (i) := paste0(get(i), " ", "(", round(get(i) / tot_pop_excluded[, get(i)] * 100, 1), "%", ")")]
+}
+
 fwrite(pop_excluded, file = paste0(direxp, "pop_excluded.csv"))
 
 rm(D3_selection_criteria, population_ins_run_in, population_no_obs_start, population_ins_run_in_sex,
    population_ins_run_in_date, population_no_obs_start_sex, population_no_obs_start_date, population_sex,
-   population_date, pop_excluded)
+   population_date, pop_excluded, tot_population_ins_run_in, tot_population_no_obs_start, tot_population_ins_run_in_sex,
+   tot_population_ins_run_in_date, tot_population_no_obs_start_sex, tot_population_no_obs_start_date, tot_population_sex,
+   tot_population_date, tot_pop_excluded)
