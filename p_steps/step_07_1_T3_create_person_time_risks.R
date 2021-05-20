3b35588b22b63513a70b008dfb01464b760c17f9
Olga Paoletti <olga.paoletti@ars.toscana.it>
2021-05-20 15:06:52 +0200

    Added countpersontime for risk and benefit

--- p_steps/step_07_1_T3_create_person_time_risks.R
+++ p_steps/step_07_1_T3_create_person_time_risks.R
@@ -1,85 +1,94 @@
 # COUNT PERSON TIME PER COVID
 #-----------------------------------------------
-#To estimate the monthly incidence rates of COVID-19 (overall and by severity level) in 2020 by data source
+#To estimate the weekly incidence rates of risks in 2020 by data source
 
-# input: D4_study_population_coprimary_c, D3_outcomes_covid.RData, list_outcomes_observed_COVID
-# output: D4_persontime_coprimary_c_month (exported to csv)
+# input: D4_study_population, D3_events_ALL_OUTCOMES.RData ,list_outcomes_observed.RData
+# output: D4_persontime_risk_week (exported to csv)
 
-print("COUNT PERSON TIME PER COVID by month for coprimary c")
 
-load(paste0(dirtemp,paste0("D3_outcomes_covid.RData")))
-load(paste0(dirpargen,paste0("list_outcomes_observed_COVID.RData")))
-load(paste0(diroutput,paste0("D4_study_population.RData")))
-load(paste0(dirpargen,"subpopulations_non_empty.RData"))
+print("COUNT PERSON TIME by week for risks")
 
-#monthly incidence
-start_persontime_studytime = "20200101"
-# first end of month after study_end
-#end_persontime_studytime =gsub("-","", study_end)
+load(paste0(dirtemp,"list_outcomes_observed.RData"))
+load(paste0(dirtemp,"D3_events_ALL_OUTCOMES.RData"))
+load(paste0(diroutput,"D4_study_population.RData"))
 
-D4_persontime_coprimary_c_month <- vector(mode = 'list')
+
+D4_persontime_risk_week <- vector(mode = 'list')
 for (subpop in subpopulations_non_empty) {  
   print(subpop)
+  start_week=seq.Date(as.Date("20200106","%Y%m%d"),Sys.Date(),by = "week")
+  
   if (this_datasource_has_subpopulations == TRUE){ 
-    study_population <- D4_study_population_coprimary_c[[subpop]]
-    events_ALL_OUTCOMES <- D3_outcomes_covid[[subpop]]
-    list_outcomes <- list_outcomes_observed_COVID[[subpop]]
+    study_population <- D4_study_population[[subpop]]
+    events_ALL_OUTCOMES <- D3_events_ALL_OUTCOMES[[subpop]]
+    list_outcomes <- list_outcomes_observed[[subpop]]
   }else{
-    study_population <- D4_study_population_coprimary_c
-    events_ALL_OUTCOMES <- D3_outcomes_covid
-    list_outcomes <- list_outcomes_observed_COVID
+    study_population <- D4_study_population
+    events_ALL_OUTCOMES <- D3_events_ALL_OUTCOMES
+    list_outcomes <- list_outcomes_observed
   }
 
-  max_exit<-study_population[,max(study_exit_date_coprimary_c)]
   
-  
+  max_exit<-study_population[,max(study_exit_date)]
   last_event<-events_ALL_OUTCOMES[,max(date_event)]
-  end_persontime_studytime<-min(max_exit,last_event)
-  
-  if (end_persontime_studytime < ymd(start_persontime_studytime)) {
+  if (last_event<ymd("20200101")) {
     next
   }
-
-  end_persontime_studytime<-as.Date(format(end_persontime_studytime, "%Y-%m-01")) + months(1) - 1
+  end_persontime_studytime<-min(max_exit,last_event)
+  start_week=start_week[start_week<=end_persontime_studytime]
+  end_week=start_week+6
+  start_week=gsub("-","", start_week)
+  end_week=gsub("-","", end_week)
   
-  end_persontime_studytime<-str_remove_all(end_persontime_studytime, "-")
-  Output_file<-CountPersonTime(
-    Dataset_events = events_ALL_OUTCOMES,
-    Dataset = study_population,
-    Person_id = "person_id",
-    Start_study_time = start_persontime_studytime,
-    End_study_time = end_persontime_studytime,
-    Start_date = "study_entry_date_coprimary_c",
-    End_date = "study_exit_date_coprimary_c",
-    Birth_date = "date_of_birth",
-    Strata = c("sex"),
-    Name_event = "name_event",
-    Date_event = "date_event",
-    Age_bands = c(0,19,29,39,49,59,69,79),
-    Increment="month",
-    Outcomes =  list_outcomes,
-    Unit_of_age = "year",
-    include_remaning_ages = T,
-    Aggregate = T
-  )
-  thisdirexp <- ifelse(this_datasource_has_subpopulations == FALSE,direxp,direxpsubpop[[subpop]])
-  fwrite(Output_file,file=paste0(thisdirexp,"D4_persontime_coprimary_c_month.csv"))
-  if (this_datasource_has_subpopulations == TRUE){ 
-    D4_persontime_coprimary_c_month[[subpop]] <- Output_file
-  }else{
-    D4_persontime_coprimary_c_month <- Output_file
+  for (i in 1:length(start_week)){
+    start_persontime_studytime = start_week[i]
+    end_persontime_studytime = end_week[i]
+    nameoutput <- paste0("Output_file",start_week[i])
+    print(nameoutput)
+    assign(nameoutput,CountPersonTime(
+      Dataset_events = events_ALL_OUTCOMES,
+      Dataset = study_population,
+      Person_id = "person_id",
+      Start_study_time = start_persontime_studytime,
+      End_study_time = end_persontime_studytime,
+      Start_date = "study_entry_date",
+      End_date = "study_exit_date",
+      Birth_date = "date_of_birth",
+      Strata = c("sex"),
+      Name_event = "name_event",
+      Date_event = "date_event",
+      Age_bands = c(0,19,29,39,49,59,69,79),
+      Increment="week",
+      Outcomes =  list_outcomes, 
+      Unit_of_age = "year",
+      include_remaning_ages = T,
+      Aggregate = T
+    )
+    )
+    if (i==1) {
+      persontime_risk_week<-get(paste0("Output_file",start_week[i]))
+      rm(list=paste0("Output_file",start_week[i]))
+    }else{
+      persontime_risk_week<-rbind(persontime_risk_week,get(paste0("Output_file",start_week[i])),fill=TRUE)
+      rm(list=paste0("Output_file",start_week[i]))
+    }
   }
-}   
+  thisdirexp <- ifelse(this_datasource_has_subpopulations == FALSE,direxp,direxpsubpop[[subpop]])
+  fwrite(persontime_risk_week,file=paste0(thisdirexp,"D4_persontime_risk_week.csv"))
+  if (this_datasource_has_subpopulations == TRUE){ 
+    D4_persontime_risk_week[[subpop]] <- persontime_risk_week
+  }else{
+    D4_persontime_risk_week <- persontime_risk_week
+  }
+}
 
-save(D4_persontime_coprimary_c_month,file=paste0(diroutput,"D4_persontime_coprimary_c_month.RData"))
-
+save(D4_persontime_risk_week,file=paste0(diroutput,"D4_persontime_risk_week.RData"))
 
 
 for (subpop in subpopulations_non_empty){
   thisdirexp <- ifelse(this_datasource_has_subpopulations == FALSE,direxp,direxpsubpop[[subpop]])
   thisdirsmallcountsremoved <- ifelse(this_datasource_has_subpopulations == FALSE,dirsmallcountsremoved,dirsmallcountsremovedsubpop[[subpop]])
-  
-  col<-colnames(Output_file)[-(1:3)]
+  col<-colnames(persontime_risk_week)[-(1:3)]
   temp<-paste0(col,"=5")
   temp2<-paste("c(",paste(temp, collapse = ','),")")
   suppressWarnings(
@@ -88,9 +97,9 @@ for (subpop in subpopulations_non_empty){
       Outputfolder = thisdirsmallcountsremoved,
       Delimiter = ",",
       Varlist = c(eval(parse(text=(temp2)))),
-      FileContains = "D4_persontime_coprimary_c_month"
+      FileContains = "D4_persontime_risk_week"
     )
   )
 }
- 
-rm(D4_study_population_coprimary_c, list_outcomes_observed_COVID,D3_outcomes_covid,D4_persontime_coprimary_c_month, Output_file, study_population , events_ALL_OUTCOMES, list_outcomes)
+# rm(list = nameobject)
+rm(D4_study_population,persontime_risk_week,study_population,events_ALL_OUTCOMES)
