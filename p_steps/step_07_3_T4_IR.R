035bf93e3cda9b7a87ef3380d0b3b1bbbdf131e2
Olga Paoletti <olga.paoletti@ars.toscana.it>
2021-05-20 15:57:32 +0200

    Draft IR computation

--- p_steps/step_07_3_T4_IR.R
+++ p_steps/step_07_3_T4_IR.R
@@ -15,12 +15,12 @@ load(paste0(diroutput,"D4_persontime_benefit_week.RData"))
 
 #for each event: "COVID_L1plus","COVID_L2plus", "COVID_L3plus","COVID_L4plus"
 
-# COVID_L1plus_b / "Persontime_COVID_L1plus" 
+#ex:  COVID_L1plus_b / "Persontime_COVID_L1plus" 
 
-D4_persontime_benefit_week <- D4_persontime_benefit_week[, IR :=  round(100000*round(person_time*1000/Persontime*Incidence, 0)/person_time, 2)]
+D4_persontime_benefit_week <- D4_persontime_benefit_week[, IR_COVID_L1plus :=  round(100000*(COVID_L1plus_b/Persontime_COVID_L1plus), 2)]
 D4_persontime_benefit_week <- D4_persontime_benefit_week[, c("lb", "ub") := list(
-  round(IR*exp(-1.96/sqrt(round(person_time*1000/Persontime*Incidence, 0))), 2),
-  round(IR*exp(+1.96/sqrt(round(person_time*1000/Persontime*Incidence, 0))), 2))]
+  round(IR_COVID_L1plus*exp(-1.96/sqrt(COVID_L1plus_b)), 2),
+  round(IR_COVID_L1plus*exp(+1.96/sqrt(COVID_L1plus_b)), 2))]
 
 
 
@@ -28,7 +28,7 @@ load(paste0(diroutput,"D4_persontime_risk_week.RData"))
 
 
 #for all outcomes
-D4_persontime_risk_week <- D4_persontime_risk_week[, IR :=  round(100000*round(person_time*1000/Persontime*Incidence, 0)/person_time, 2)]
-D4_persontime_risk_week <- D4_persontime_risk_week[, c("lb", "ub") := list(
-  round(IR*exp(-1.96/sqrt(round(person_time*1000/Persontime*Incidence, 0))), 2),
-  round(IR*exp(+1.96/sqrt(round(person_time*1000/Persontime*Incidence, 0))), 2))]
+# D4_persontime_risk_week <- D4_persontime_risk_week[, IR :=  round(100000*round(person_time*1000/Persontime*Incidence, 0)/person_time, 2)]
+# D4_persontime_risk_week <- D4_persontime_risk_week[, c("lb", "ub") := list(
+#   round(IR*exp(-1.96/sqrt(round(person_time*1000/Persontime*Incidence, 0))), 2),
+#   round(IR*exp(+1.96/sqrt(round(person_time*1000/Persontime*Incidence, 0))), 2))]
