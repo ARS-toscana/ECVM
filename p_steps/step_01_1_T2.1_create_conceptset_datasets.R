61024a76b064e9ae919635b95f9b54a15144cc78
Davide <davide.messina@ars.toscana.it>
2021-06-17 16:48:53 +0200

    removed cicles for

--- p_steps/step_01_1_T2.1_create_conceptset_datasets.R
+++ p_steps/step_01_1_T2.1_create_conceptset_datasets.R
@@ -20,84 +20,22 @@ CreateConceptSetDatasets(concept_set_names = c(vaccine__conceptssets),
                          extension = c("csv")
                          )
 
+CreateConceptSetDatasets(concept_set_names = c(OUTCOMES_conceptssets, COV_conceptssets, DRUGS_conceptssets, SEVERCOVID_conceptsets),
+                         dataset = ECVM_CDM_tables,
+                         codvar = ECVM_CDM_codvar,
+                         datevar = ECVM_CDM_datevar,
+                         EAVtables = ECVM_CDM_EAV_tables,
+                         EAVattributes = ECVM_CDM_EAV_attributes_this_datasource,
+                         dateformat= "YYYYmmdd",
+                         vocabulary = ECVM_CDM_coding_system_cols,
+                         rename_col = list(person_id=person_id,date=date),
+                         concept_set_domains = concept_set_domains,
+                         concept_set_codes =	concept_set_codes_our_study,
+                         concept_set_codes_excl = concept_set_codes_our_study_excl,
+                         discard_from_environment = T,
+                         dirinput = dirinput,
+                         diroutput = dirtemp,
+                         extension = c("csv"),
+                         vocabularies_with_dot_wildcard=c("READ"))
 
-# APPLY THE FUNCTION CreateConceptSetDatasets TO CREATE ONE DATASET PER CONCEPT SET CONTAINING ONLY RECORDS WITH A CODE OF INTEREST
-for (conceptset in OUTCOMES_conceptssets){
-  CreateConceptSetDatasets(concept_set_names = conceptset,
-                           dataset = ECVM_CDM_tables,
-                           codvar = ECVM_CDM_codvar,
-                           datevar = ECVM_CDM_datevar,
-                           EAVtables = ECVM_CDM_EAV_tables,
-                           EAVattributes = ECVM_CDM_EAV_attributes_this_datasource,
-                           dateformat= "YYYYmmdd",
-                           vocabulary = ECVM_CDM_coding_system_cols,
-                           rename_col = list(person_id=person_id,date=date),
-                           concept_set_domains = concept_set_domains,
-                           concept_set_codes =	concept_set_codes_our_study,
-                           concept_set_codes_excl = concept_set_codes_our_study_excl,
-                           discard_from_environment = T,
-                           dirinput = dirinput,
-                           diroutput = dirtemp,
-                           extension = c("csv"),
-                           vocabularies_with_dot_wildcard=c("READ"))
-}
-
-for (conceptset in COV_conceptssets){
-  CreateConceptSetDatasets(concept_set_names = conceptset,
-                           dataset = ECVM_CDM_tables,
-                           codvar = ECVM_CDM_codvar,
-                           datevar= ECVM_CDM_datevar,
-                           EAVtables = ECVM_CDM_EAV_tables,
-                           EAVattributes = ECVM_CDM_EAV_attributes_this_datasource,
-                           dateformat = "YYYYmmdd",
-                           vocabulary = ECVM_CDM_coding_system_cols,
-                           rename_col = list(person_id=person_id,date=date),
-                           concept_set_domains = concept_set_domains,
-                           concept_set_codes =	concept_set_codes_our_study,
-                           concept_set_codes_excl = concept_set_codes_our_study_excl,
-                           discard_from_environment = T,
-                           dirinput = dirinput,
-                           diroutput = dirtemp,
-                           extension = c("csv"),
-                           vocabularies_with_dot_wildcard = c("READ"))
-}
-
-for (conceptset in DRUGS_conceptssets){
-  CreateConceptSetDatasets(concept_set_names = conceptset,
-                           dataset = ECVM_CDM_tables,
-                           codvar = ECVM_CDM_codvar,
-                           datevar = ECVM_CDM_datevar,
-                           EAVtables = ECVM_CDM_EAV_tables,
-                           EAVattributes = ECVM_CDM_EAV_attributes_this_datasource,
-                           dateformat = "YYYYmmdd",
-                           vocabulary = ECVM_CDM_coding_system_cols,
-                           rename_col = list(person_id=person_id,date=date),
-                           concept_set_domains = concept_set_domains,
-                           concept_set_codes =	concept_set_codes_our_study,
-                           concept_set_codes_excl = concept_set_codes_our_study_excl,
-                           discard_from_environment = T,
-                           dirinput = dirinput,
-                           diroutput = dirtemp,
-                           extension = c("csv"))
-}
-
-for (conceptset in SEVERCOVID_conceptsets){
-  CreateConceptSetDatasets(concept_set_names = conceptset,
-                           dataset = ECVM_CDM_tables,
-                           codvar = ECVM_CDM_codvar,
-                           datevar= ECVM_CDM_datevar,
-                           EAVtables=ECVM_CDM_EAV_tables,
-                           EAVattributes= ECVM_CDM_EAV_attributes_this_datasource,
-                           dateformat= "YYYYmmdd",
-                           vocabulary = ECVM_CDM_coding_system_cols,
-                           rename_col = list(person_id=person_id,date=date),
-                           concept_set_domains = concept_set_domains,
-                           concept_set_codes =	concept_set_codes_our_study,
-                           concept_set_codes_excl = concept_set_codes_our_study_excl,
-                           discard_from_environment = T,
-                           dirinput = dirinput,
-                           diroutput = dirtemp,
-                           extension = c("csv"),
-                           vocabularies_with_dot_wildcard = c("READ"))
-}
 
