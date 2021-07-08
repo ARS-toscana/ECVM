33df5cec3bf7d1b50588256741f53ed42c5bf4c3
RosaMGini <rosa.gini@ars.toscana.it>
2021-07-08 16:02:38 +0200

    version 4.2

--- to_run.R
+++ to_run.R
@@ -1,8 +1,11 @@
 #-------------------------------
 # ECVM script
-# v4.0 - 28 June 2021
+# v4.2 - 8 July 2021
 # authors: Rosa Gini, Olga Paoletti, Davide Messina, Giorgio Limoncella
 
+# changelog V4.2:
+# new codes ICPC and italian ICD9 codes
+# improved BIFAP covid registry
 
 # changelog V4.0:
 # included ACCESS codes
